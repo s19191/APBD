e6625eed24296f7014389df9832e6c402eb147c1
s19191 <s19191@pjwstk.edu.pl>
2020-03-17 10:04:46 +0100

    Stan po ćwiczeniach, bez podpunktów 7 oraz 8

---
+++ cw3/cw3/Controllers/StudentsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using cw3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        public string GetStudent(string orderBy)
+        {
+            return $"Kowalski, Malewski, Andrzejewski srortowanie={orderBy}";
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudnet(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+        [HttpPost]
+        public IActionResult CreateStudent(Studnet studnet)
+        {
+            studnet.IndexNumber = $"s{new Random().Next(1, 2000)}";
+            return Ok(studnet);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteStudnet(Studnet studnet)
+        {
+            return Ok("200 Usuwanie ukończone");
+        }
+
+        [HttpPut]
+        public IActionResult PutStudnet(Studnet studnet)
+        {
+            return Ok("200 Aktualizacja dokończona");
+        }
+    }
+}
