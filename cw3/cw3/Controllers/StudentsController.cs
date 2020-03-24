98e0ca0f3ae4ba29a6b1530a70e7ddc0473633ad
s19191 <s19191@pjwstk.edu.pl>
2020-03-24 14:50:44 +0100

    stan po ćwiczeniach, zrobione do 4.2 włącznie

--- cw3/cw3/Controllers/StudentsController.cs
+++ cw3/cw3/Controllers/StudentsController.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
 using cw3.DAL;
 using cw3.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -10,43 +13,56 @@ namespace cw3.Controllers
     public class StudentsController : ControllerBase
     {
         private readonly IDbService _dbService;
+        private const string ConString = "Data Source=db-mssql;Initial Catalog=s19191;Integrated Security=True";
 
         public StudentsController(IDbService dbService)
         {
             _dbService = dbService;
         }
         
-        public string GetStudent(string orderBy)
-        {
-            return $"Kowalski, Malewski, Andrzejewski srortowanie={orderBy}";
-        }
+        // public string GetStudent(string orderBy)
+        // {
+        //     return $"Kowalski, Malewski, Andrzejewski srortowanie={orderBy}";
+        // }
 
+        // [HttpGet]
+        // public IActionResult GetStudents(string orderBy)
+        // {
+        //     return Ok(_dbService.GetStudents());
+        // }
+        
         [HttpGet]
-        public IActionResult GetStudents(string orderBy)
+        public IActionResult GetStudnet()
         {
-            return Ok(_dbService.GetStudents());
+            List<Studnet> studnets = new List<Studnet>();
+            using (SqlConnection con = new SqlConnection(ConString))
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "select * from Student inner join Enrollment on Student.IdEnrollment=Enrollment.IdEnrollment inner join Studies on Enrollment.IdStudy=Studies.IdStudy";
+                
+                con.Open();
+                SqlDataReader dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    Studnet studnet = new Studnet();
+                    studnet.FirstName = dr["FirstName"].ToString();
+                    studnet.LastName = dr["LastName"].ToString();
+                    studnet.BirthDate = DateTime.Parse(dr["BirthDate"].ToString());
+                    studnet.nazwaStudniow = dr["Name"].ToString();
+                    studnet.Semester = (int) dr["Semester"];
+                    studnets.Add(studnet);
+                }
+            }
+            return Ok(studnets);
         }
         
-        [HttpGet("{id}")]
-        public IActionResult GetStudnet(int id)
-        {
-            if (id == 1)
-            {
-                return Ok("Kowalski");
-            }
-            else if (id == 2)
-            {
-                return Ok("Malewski");
-            }
-            return NotFound("Nie znaleziono studenta");
-        }
-        
-        [HttpPost]
-        public IActionResult CreateStudent(Studnet studnet)
-        {
-            studnet.IndexNumber = $"s{new Random().Next(1, 2000)}";
-            return Ok(studnet);
-        }
+        // [HttpPost]
+        // public IActionResult CreateStudent(Studnet studnet)
+        // {
+        //     studnet.IndexNumber = $"s{new Random().Next(1, 2000)}";
+        //     return Ok(studnet);
+        // }
 
         [HttpPut("{id}")]
         public IActionResult putStudent(int id)
--- cw3/cw3/Models/Studnet.cs
+++ cw3/cw3/Models/Studnet.cs
@@ -5,9 +5,15 @@ namespace cw3.Models
 {
     public class Studnet
     {
-        public int IdStudent { get; set; }
+        //public int IdStudent { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string IndexNumber { get; set; }
+        // public string IndexNumber { get; set; }
+        
+        public DateTime BirthDate { get; set; }
+        
+        public string nazwaStudniow { get; set; }
+        
+        public int Semester { get; set; }
     }
 }
