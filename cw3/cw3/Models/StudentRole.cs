bbce4b8e9bbc506bca9294c13b6886e867624ada
s19191 <s19191@pjwstk.edu.pl>
2020-05-13 23:31:50 +0200

    dodane te klasy z bazy database first

---
+++ cw3/cw3/Models/StudentRole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cw3.Models
+{
+    public partial class StudentRole
+    {
+        public int? IndexNumber { get; set; }
+        public int? IdRole { get; set; }
+    }
+}
