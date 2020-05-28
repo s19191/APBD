a68a04ae973ff650010c088d214eb6164f7871e0
s19191 <s19191@pjwstk.edu.pl>
2020-05-29 01:50:16 +0200

    coś dodane

--- cw12/cw12/Startup.cs
+++ cw12/cw12/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MvcMovie.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace cw12
 {
@@ -24,6 +26,9 @@ namespace cw12
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
