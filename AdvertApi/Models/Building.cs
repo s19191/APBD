5a50ce6eb47625cb789fcb74ce0cf0363c0c97fb
s19191 <s19191@pjwstk.edu.pl>
2020-06-20 20:46:56 +0200

    początek pracy nad projektem, już są modele z podejścia DataBaseFirst, sql generujący bazę w folderze

---
+++ AdvertApi/Models/Building.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdvertApi.Models
+{
+    public partial class Building
+    {
+        public Building()
+        {
+            CampaignFromBuildingNavigation = new HashSet<Campaign>();
+            CampaignToBuildingNavigation = new HashSet<Campaign>();
+        }
+
+        public int IdBuilding { get; set; }
+        public string Street { get; set; }
+        public int StreetNumber { get; set; }
+        public string City { get; set; }
+        public decimal Height { get; set; }
+
+        public virtual ICollection<Campaign> CampaignFromBuildingNavigation { get; set; }
+        public virtual ICollection<Campaign> CampaignToBuildingNavigation { get; set; }
+    }
+}
