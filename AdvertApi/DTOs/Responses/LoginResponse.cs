9abbaada7a704e603fc479f8423ec3d7ac1b0226
s19191 <s19191@pjwstk.edu.pl>
2020-06-22 00:09:18 +0200

    sprzątanie, nadal nie ma testów, są one dość dziwne

---
+++ AdvertApi/DTOs/Responses/LoginResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace AdvertApi.DTOs.Responses
+{
+    public class LoginRespone
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public LoginRespone(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public override string ToString()
+        {
+            return "Klient zalogowany prawidłowo";
+        }
+    }
+}
