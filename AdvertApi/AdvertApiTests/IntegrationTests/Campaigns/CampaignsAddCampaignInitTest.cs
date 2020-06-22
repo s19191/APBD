e5a4ecda1d544d7dd6fa5668c1df227cd0d5842d
s19191 <s19191@pjwstk.edu.pl>
2020-06-22 15:29:31 +0200

    dodany 1 unit test, ale nie działa bo nie wiem jak zrobić configurtion z secretem

--- AdvertApi/AdvertApiTests/IntegrationTests/Campaigns/CampaignsAddCampaignInitTest.cs
+++ AdvertApi/AdvertApiTests/IntegrationTests/Campaigns/CampaignsAddCampaignInitTest.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NUnit.Framework;
 
 namespace AdvertApiTests.IntegrationTests.Campaigns
 {
+    [TestFixture]
     class CampaignsAddCampaignInitTest
     {
+        [Test]
+        public void AddCampaignMethod_CompleteRequest_Correct()
+        {
+            
+        }
     }
 }
--- AdvertApi/AdvertApiTests/UniTests/Clients/ClientRefreshTokenUnitTest.cs
+++ AdvertApi/AdvertApiTests/UniTests/Clients/ClientRefreshTokenUnitTest.cs
@@ -1,10 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AdvertApi.Controllers;
+using AdvertApi.Models;
+using AdvertApi.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Moq;
+using NUnit.Framework;
 
 namespace AdvertApiTests.UniTests.Clients
 {
+    [TestFixture]
     class ClientRefreshTokenUnitTest
     {
+        [Test]
+        public void RefreshTokenMethod_CompleteRequest_Correct()
+        {
+            var dbLayer = new Mock<IAdvertDbService>();
+            dbLayer.Setup(d => d.checkRefreshToken("tmp")).Returns(new Client
+            {
+                IdClient = 1,
+                FirstName = "Ala",
+                LastName = "MaKota",
+                Email = "ala@ma.Kota",
+                Phone = "niema",
+                Login = "login1",
+                Password = "mamKota123",
+                Salt = "tmpSalt",
+                RefreshToken = "tmpRefreshToken"
+            });
+            
+            //var cont = new ClientsController(new ConfigurationBuilder().Add(new JsonConfigurationSource{}).Build(), dbLayer.Object);
+            var cont = new ClientsController(new ConfigurationBuilder().Build(), dbLayer.Object);
+            
+            var result = cont.RefreshToken("tmp");
+            
+            Assert.IsNotNull(result);
+        }
     }
 }
